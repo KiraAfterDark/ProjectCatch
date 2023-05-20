7881ffc7bffcefa1ea50c13c0fa020e2c97bc383
Sam Dinan <samdinan@gmail.com>
2023-05-20 15:50:38 -0300

    Putting character builder on hold. I want to implement ther Unity loc system

--- Assets/Scripts/TrainerBuilders/TrainerBuilderAsset.cs
+++ Assets/Scripts/TrainerBuilders/TrainerBuilderAsset.cs
@@ -7,8 +7,19 @@ namespace ProjectCatch.TrainerBuilders
     public class TrainerBuilderAsset : MonoBehaviour
     {
         [Title("Map Assets")]
-        
+
+        [Required]
         [SerializeField]
-        private MapTrainer
+        private MapTrainer mapTrainer;
+
+        public MapTrainer MapTrainer => mapTrainer;
+
+        [Title("Battle Assets")]
+
+        [Required]
+        [SerializeField]
+        private GameObject battleMesh;
+
+        public GameObject BattleMesh => battleMesh;
     }
 }
